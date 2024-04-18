802491f0a7361c1dc34fa9826c6fb99c486118bb
James Kinyanjui <james.kinyanjui@principia.edu>
2024-04-18 10:23:05 -0500

    changes for adding testing

---
+++ __tests__/regression/basic.test.js
@@ -0,0 +1,17 @@
+const request = require('supertest');
+const {app} = require('../../src/app');
+
+describe('GET /', () => {
+
+   beforeEach(() => {
+      fetch.mockClear();
+   });
+
+   it('Respond with default', async() => {
+      fetch.mockResponse(JSON.stringify([{id: 1, name: 'Falcon 9'}]));
+
+      const response = await request(app).get('/');  
+      expect(response.statusCode).toBe(200);
+      expect(response.text).toContain('Falcon 9');
+   });
+});
