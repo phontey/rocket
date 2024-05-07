f5ba01b84dfa935bb581155149d463aebd194b75
James Kinyanjui <james.kinyanjui@principia.edu>
2024-05-06 20:42:27 -0500

    fixed port

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const fetch = require('node-fetch'); 
 const app = express();
 const path = require('path');
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
